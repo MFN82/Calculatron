25b1b241ab3da66e40f974c0685c273543aecd32
Martijn Nahumury <Martijn@MacBook-Pro-van-Martijn.local>
2016-03-17 11:43:57 -0500

    add outlets

--- Calculatron/ViewController.m
+++ Calculatron/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *addEndText;
+@property (weak, nonatomic) IBOutlet UITextField *addEndTextTwo;
+@property (weak, nonatomic) IBOutlet UILabel *operationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
 
 @end
 
@@ -18,10 +22,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onButtonTapped:(UIButton *)sender {
 }
 
 @end
